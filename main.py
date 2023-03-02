e11230fa5faa29fee987ba2ece4f746011f0fe29
aalvaropc <aalvaropc@gmail.com>
2023-03-01 21:42:02 -0500

    intento

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import tkinter as tk
 from functions import *
+from tkinter import filedialog
+from tkinter import messagebox
+import os
+
+CLAVES = "claves.sdf"
+RESPUESTAS = "respuestas.sdf"
+IDENTIFI= "identifi.sdf"
 NAV_BG = 'gray'
 BTN_BG = 'gray'
 BTN_FG = 'white'
@@ -29,11 +36,13 @@ class Navbar(tk.Frame):
         self.validation_panel.config(bg=PANEL_BG, height=PANEL_HEIGHT, width=PANEL_WIDTH)
         self.qualify_panel.config(bg=PANEL_BG, height=PANEL_HEIGHT, width=PANEL_WIDTH)
         
+        self.path_label = tk.Label(self.qualify_panel, text="")
+        self.path_label.grid(row=4, column=1)
         # Widgets Archivos
         
-        tk.Button(self.file_panel, text='Subir claves', width=20, height=2, command=upload_keys).grid(row=0, column=0, padx=(100,50), pady=50)
-        tk.Button(self.file_panel, text='Cargar Identificadores', width=20, height=2).grid(row=1, column=0, padx=(100,50), pady=50)
-        tk.Button(self.file_panel, text='Cargar respuestas', width=20, height=2).grid(row=2, column=0, padx=(100,50), pady=50)
+        tk.Button(self.file_panel, text='Subir claves', width=20, height=2, command=self.select_folder, ).grid(row=0, column=0, padx=(100,50), pady=50)
+        tk.Button(self.file_panel, text='Cargar Identificadores', width=20, height=2, command=self.select_folder).grid(row=1, column=0, padx=(100,50), pady=50)
+        tk.Button(self.file_panel, text='Cargar respuestas', width=20, height=2, command=self.select_folder).grid(row=2, column=0, padx=(100,50), pady=50)
         tk.Button(self.file_panel, text='Validar', width=20, height=2).grid(row=3, column=0, padx=(100,50), pady=50)
 
         self.file_entry = tk.Text(self.file_panel, width=40, height=30)
@@ -43,7 +52,7 @@ class Navbar(tk.Frame):
 
         # Widgets Validacion
 
-        tk.Button(self.validation_panel, text='Validar estructura', width=20, height=2, command=upload_keys).grid(row=0, column=0, padx=(100,50), pady=20)
+        tk.Button(self.validation_panel, text='Validar estructura', width=20, height=2).grid(row=0, column=0, padx=(100,50), pady=20)
         tk.Button(self.validation_panel, text='Validar codigos duplicados', width=20, height=2).grid(row=1, column=0, padx=(100,50), pady=20)
         tk.Button(self.validation_panel, text='Validar duplicados de litos', width=20, height=2).grid(row=2, column=0, padx=(100,50), pady=20)
         tk.Button(self.validation_panel, text='Validar carnet postulante', width=20, height=2).grid(row=3, column=0, padx=(100,50), pady=20)
@@ -55,7 +64,7 @@ class Navbar(tk.Frame):
         
         # Widgets Calificador
 
-        tk.Button(self.qualify_panel, text='Calificar normal', width=20, height=2, command=upload_keys).grid(row=0, column=0, padx=(100,50), pady=50)
+        tk.Button(self.qualify_panel, text='Calificar normal', width=20, height=2).grid(row=0, column=0, padx=(100,50), pady=50)
         tk.Button(self.qualify_panel, text='Calificar anonima', width=20, height=2).grid(row=1, column=0, padx=(100,50), pady=50)
         tk.Button(self.qualify_panel, text='Calificar ambos', width=20, height=2).grid(row=2, column=0, padx=(100,50), pady=50)
         tk.Button(self.qualify_panel, text='Guardar en..', width=20, height=2).grid(row=3, column=0, padx=(100,50), pady=50)
@@ -68,6 +77,13 @@ class Navbar(tk.Frame):
         self.file_panel.pack_forget()
         self.validation_panel.pack_forget()
         self.qualify_panel.pack_forget()
+
+    def select_folder(self, text):
+        folder_path = filedialog.askdirectory()
+        ruta_archivo = os.path.join(folder_path, text)
+        if ruta_archivo != "":
+            messagebox.showerror("Error", f"{ruta_archivo}")
+            return
 
     def show_file(self):
         self.file_panel.pack(side='top', fill='both', expand=True)
@@ -84,7 +100,6 @@ class Navbar(tk.Frame):
         self.file_panel.pack_forget()
         self.validation_panel.pack_forget()
         self.qualify_panel.pack(side='top', fill='both', expand=True)
-
 
 root = tk.Tk()
 root.title("AdminUnica")
