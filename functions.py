101dcde52e3b27d37e5b49dd85d436082bd30933
Ausubel <intelectus61@gmail.com>
2023-03-09 08:48:10 -0500

    eliminando modulo functions

--- functions.py
+++
@@ -1,6 +0,0 @@
-from main import ruta_archivo
-def read_file():
-    with open(ruta_archivo, "r") as archivo:
-        # Realizar operaciones en el archivo
-        contenido = archivo.read()
-        print(contenido)
